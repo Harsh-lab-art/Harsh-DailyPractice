ac234e2660e03273a63a308fd7068a1875f4222e
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-01-13 20:57:29 +0530

    Foreachloop.java

---
+++ Foreachloop.java
@@ -0,0 +1,16 @@
+class Foreachloop 
+{
+    public static void main(String args[])
+    {
+        int[]arr={1,2,3,4,5,6,7,8,9};
+        for(int n:arr)
+        {
+            System.out.println("my name is verma");
+        }
+        String []m={"apple","mango","banana","litchi"};
+        for(String j:m)
+        {
+            System.out.println(j);
+        }
+    }
+}
