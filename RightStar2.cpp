b4f5742a9573d04826d8c46c7456e6d85ef497c7
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-01-22 15:42:46 +0530

    RightStar2.cpp

---
+++ RightStar2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the no. coloumns";
+    cin>>n;
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=i;j>=1;j--)
+        {
+            cout<<"*";
+        }
+        cout<<" "<<endl;
+    }
+    return 0;
+}
